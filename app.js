c71c936c4e7678bb3b66e795b4e792576b6bcb13
samuel21695 <samuel21695@gmail.com>
2023-10-30 12:29:53 +0900

    create API 엔드포인트

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const app = express()
 // 정적 파일 (HTML, CSS, Javascript0)을 제공하기 위해 'public' 디렉토리 설정
 app.use(express.static('public'))
 
+// API 엔드포인트 생성
+app.get('/api/data', (req, res) => {
+  // 이 API는 "데이터 창고"와 같습니다. 데이터를 요청하면 창고에서 데이터를 꺼내 반환합니다.
+  const data = {message: 'Hello, Ajax! This is a data from the server.'}
+  res.json(data);
+})
