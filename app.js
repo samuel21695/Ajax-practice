88458a8ba7c2ed336f3cdf6b08d5607df7b4653f
samuel21695 <samuel21695@gmail.com>
2023-10-30 10:37:26 +0900

    xhr.open()

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // XMLHttpRequest 객체 생성
 const xhr = new XMLHttpRequest();
+
+// 요청을 열고 설정
+// xhr.open(method, url, async)
+// method: HTTP 요청 메서드를 지정합니다. 주로 "GET" 또는 "POST"를 사용합니다.
+xhr.open('GET', 'https://api.example.com/data', true);
